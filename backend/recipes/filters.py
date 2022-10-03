6896c21fca33eb06b3328ad897db8d15aed4d861
Никита Россиин <n.rossiin98@yandex.ru>
2022-10-03 21:08:52 +0300

    правки

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,4 +1,4 @@
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
 from .models import Recipe
@@ -8,7 +8,7 @@ class IngredientSearchFilter(SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilter(filters.FilterSet):
+class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
